9cf00e7c56bca7dfb2bffe3af031505f7bfaef78
Shrey Gupta <connectwithshrey@gmail.com>
2019-02-28 01:26:29 +0530

    Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8000/'
+  apiBaseUrl: 'http://contacts.shrey.work:8080/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8000/'
+  apiBaseUrl: 'http://contacts.shrey.work:8080/'
 };
 
 /*
